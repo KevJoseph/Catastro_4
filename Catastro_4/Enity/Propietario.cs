9b716f9c49865aef91a72f42a6e80b38ea564813
Kev <kevin.ramos@undc.edu.pe>
2018-04-16 12:19:24 -0500

    Raiz

---
+++ Catastro_4/Enity/Propietario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catastro_4.Enity
+{
+    class Propietario
+    {
+        static string Main(string[] args)
+        {
+            // Display the number of command line arguments:
+            Console.WriteLine("Hola Mundo");
+            return "";
+        }
+
+    }
+
+}
