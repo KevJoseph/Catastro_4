0709cf4b9a248319d9e304c6d1c6cb7ac936db62
Kev <kevin.ramos@undc.edu.pe>
2018-04-16 14:16:41 -0500

    Clase BaseDatos

---
+++ Catastro_4/Basedatos/BaseDatos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catastro_4.Basedatos
+{
+    class BaseDatos
+    {
+        // coloca tus datos de tu equipo ( que va actuar como servidor)
+        private string servidor = "Informatica\\SQLEXPRESS";
+        private string data_base = "catastro_Demo";
+        private string cadena;
+        public SqlConnection conexion = new SqlConnection();
+
+        public BaseDatos()
+        {
+            this.cadena = "Data Source="+this.servidor+";Initial Catalog="+this.data_base+"; Integrated Security=True";
+        }
+
+        public void Open()
+        {
+            try
+            {
+                conexion.Open();
+                Console.WriteLine("Conexion abierta con la base de datos.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al conectar con la base de datos.");
+            }
+
+        }
+
+        public void close()
+        {
+            conexion.Close();
+        }
+    }
+}
